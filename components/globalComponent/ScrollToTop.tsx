0e8f7a7276898494b51b580617ba0fe696258ff3
vikash0p <vikash2000158@gmail.com>
2025-04-08 20:37:03 +0530

    update ScrollToTop button styles to improve visibility; change background color logic based on visibility state

--- components/globalComponent/ScrollToTop.tsx
+++ components/globalComponent/ScrollToTop.tsx
@@ -26,8 +26,8 @@ const ScrollToTop = () => {
 		<div>
 			<button
 				type='button'
-				className={`fixed bottom-4 right-4  text-white px-4 py-2 rounded-full shadow-lg hover:bg-custom-2 transition-colors duration-300 ${
-					isVisible && 'bg-custom-1'
+				className={`fixed bottom-4 right-4   px-4 py-4 rounded-full shadow-lg hover:bg-custom-2 transition-colors duration-300 ${
+					isVisible ? 'bg-custom-1' : 'bg-gray-400'
 				}`}
 				onClick={scrollToTop}
 			>
