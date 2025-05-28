c64329f74de3d843a46f6e8a414c925dda33b7e6
vikash0p <vikash2000158@gmail.com>
2025-05-28 11:11:45 +0530

    feat: Add new icons to manifest for improved branding and user experience

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,7 +14,25 @@ export default function manifest() {
     dir: 'ltr',
     categories: ['ecommerce', 'furniture', 'home decor', 'interior design'],
 
-    
+    icons: [
+      {
+        src: '/favicon/icon.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+
     shortcuts: [
       // 🌟 Shopping Experience
       {
