d76d04f2a05f459b5347dd3e20c484519b2edcad
praveen804 <seggamnagapraveen@gmail.com>
2025-10-26 21:07:45 +0530

    commi

--- app/(root)/(authNavigation)/cart/page.tsx
+++ app/(root)/(authNavigation)/cart/page.tsx
@@ -1,7 +1,9 @@
-/* export const metadata = CartMeta;
+import CartContainer from '@/components/cartComponents/CartContainer';
+import { CartMeta } from '@/src/meta/CartMeta';
+import React from 'react';
+export const metadata = CartMeta;
 const Cart = () => {
   return <CartContainer />;
 };
 
 export default Cart;
-*/
